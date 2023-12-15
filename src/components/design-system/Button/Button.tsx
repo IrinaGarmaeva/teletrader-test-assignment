574b4665f6366ee0e9d03b028778ad847133b7ed
Владислав Филатов <filatov@reg.ru>
2023-12-15 21:54:28 +0700

    typescript: corrected type in favoutite tsx file

--- src/components/design-system/Button/Button.tsx
+++ src/components/design-system/Button/Button.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { ButtonProps } from './types';
 
 function Button({
   className, type, onClick, text,
-}) {
+}: ButtonProps) {
   return (
-    // eslint-disable-next-line react/button-has-type
     <button className={`${className} button`} type={type} onClick={onClick}>
       {text}
     </button>
